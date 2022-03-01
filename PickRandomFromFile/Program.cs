44a47a32fd26bbd43f6e8ee7a4ad9a237100cec6
rvesk <jksdgn@gmail.com>
2022-03-01 04:27:22 -0500

    Control Flow

---
+++ PickRandomFromFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace PickRandomFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\rvesk\samples\foods.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+
+            foreach(string element in dataFromFile)
+
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
